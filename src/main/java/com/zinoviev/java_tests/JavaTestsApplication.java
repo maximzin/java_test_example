8a1548d25a8bc7d874d5bada84bfa82c200ee0ee
maxim zinovev <maximzin@mail.ru>
2025-03-26 19:46:56 +0500

    first commit

---
+++ src/main/java/com/zinoviev/java_tests/JavaTestsApplication.java
@@ -0,0 +1,13 @@
+package com.zinoviev.java_tests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTestsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaTestsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zinoviev/java_tests/JavaTestsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zinoviev.java_tests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTestsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
